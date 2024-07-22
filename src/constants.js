00d7436b44ef2a00f77df3aa5c04ba1502450182
Drishti Malavade <102899430+DrishtiMalavade@users.noreply.github.com>
2024-07-22 11:50:54 +0530

    constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,15 @@
+export const scaleFactor = 4;
 
+export const dialogueData = {
+  pc: `This is my PC. These days im working with Javascript and some game dev.
+    I've made a couple of games in js and python. I also love Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/DrishtiMalavade" target="_blank">Github</a>!`,
+  cs: `This is my CS degree. I am proud of it.`,
+  tv: `That's my TV. I like watching various stuff. My favourite movies include Whiplash, Truman show and Interstellar`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="link" target="_blank">Check it out?</a>
+  Contact me at drishti.malavade02@gmail.com if you have any interesting job opportunities!`,
+  books: `I love reading a lot. Maybe someday I will have a vast library. I like acquiring new knowledge about various things`,
+  exit: `If you want to exit, just close the tab.`,
+  garden: `This is my garden, I love gardening and I worked hard on these plants. It gives me a sense of peace, a break from this busy world.`,
+};
